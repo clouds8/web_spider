359490c07e43bced4ff37efc9b8ab92dca598bb7
clouds8 <clouds8@sina.cn>
2016-07-17 03:44:08 +0800

    完成各个流程，待优化代码

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const async = require('async');
 const superagent = require('superagent');
 const eventproxy = require('eventproxy');
 const request = require('request');
+// const request = require('request').defaults({'proxy': 'http://myproxy:1234/'});
 const url = require('url');
 const path = require('path');
 
@@ -15,22 +16,8 @@ let hostPath = 'http://idzn.net/angular/';
 // let indexPageUrls = [];   // 正则表达式匹配网页引用的文件    ("|')[^('|")]+\.(js|css|html)(?='|")
 let indexPageName = 'index.html'
 let downloadDir = 'catch_web/';
-
-// let preWorkEP = new eventproxy
-
-// preWorkEP.all('getIndexPageUrls', 'makeStoreFile', 'createIndexPage', function(){
-//
-// });
-// preWorkEP.fail(function (err) {
-//   console.error(err);
-// });
-//
-// superagent
-//   .get(target)
-//   .end(preWorkEP.done('getIndexPageUrls', data){
-//
-//     return indexPageUrls;
-//   })
+let limit = 5;
+let hideFilesRelativeUrls = ['js/config.lazyload.js', 'js/config.router.js'];
 
 async.waterfall([
   createDownloadDir,
@@ -54,9 +41,9 @@ function createDownloadDir(callback) {
   console.log('createDownloadDir');
   fs.exists(downloadDir, function (exist) {
     if (exist) {
-      callback();
+      callback(null);
     } else {
-      fs.mkdir(downloadDir, callback());
+      fs.mkdir(downloadDir, callback(err));
     }
   });
 }
@@ -70,7 +57,7 @@ function fetchTargetIndex(callback) {
         console.log('catch fail');
         callback(err);
       } else {
-        console.log('catch success');
+        // console.log('catch success');
         let indexPage = data.text;
         // console.log(indexPage);
         let indexPageUrls = indexPage.match(/("|')[^('|")]+\.(js|css|html)(?='|")/g);
@@ -82,13 +69,13 @@ function fetchTargetIndex(callback) {
         }
         fs.writeFile(downloadDir + indexPageName, indexPage, function (err) {
           if (err) {
-            console.log('writeFile err');
+            // console.log('writeFile err');
             callback(err);
           } else {
-            console.log('write successfully');
+            // console.log('write successfully');
             // console.log(downloadDir);
             // console.log(indexPageUrls);
-            callback(null, downloadDir, indexPageUrls);
+            callback(null, indexPageUrls);
           }
         });
       }
@@ -96,13 +83,13 @@ function fetchTargetIndex(callback) {
     console.log('fetchTargetIndex complete');
 }
 
-function makeDirsForMain(downloadDir, indexPageUrls, callback){
+function makeDirsForMain(indexPageUrls, callback){
   console.log('makeDirsForMain');
   let urlObjs = makeUrlObjs(hostPath, srcBase, indexPageUrls);
   // console.log(urlObjs);
   async.each(urlObjs, function (urlObj, callback) {
     let dirPath = downloadDir + urlObj.absoluteDir;
-    console.log(dirPath);
+    // console.log(dirPath);
     mkdirs(dirPath, function (err) {
       console.log('make end');
       callback(err)
@@ -112,29 +99,94 @@ function makeDirsForMain(downloadDir, indexPageUrls, callback){
       callback(err);
     } else {
       console.log('makeDirsForMain end');
-      callback(null, downloadDir, urlObjs)
+      callback(null, urlObjs)
     }
   });
 }
 
-function downloadMain(downloadDir, indexPageUrls, callback) {
+function downloadMain(urlObjs, callback) {
   console.log('downloadMain start');
-  callback(null, 233);
+  download(downloadDir, limit, urlObjs, function (err) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
+  // callback(null, 233);
 }
 
-function getRemain(downloadDir, callback) {
+function getRemain(callback) {
   console.log('getRemain start');
-  callback(null, 122, 123)
+  let urlObjs = makeUrlObjs(hostPath, srcBase, hideFilesRelativeUrls);
+  async.map(urlObjs, function (urlObj, callback) {
+    console.log('the urlObj is :');
+    console.log(urlObj);
+    console.log(downloadDir + urlObj.absoluteUrl);
+    fs.readFile(downloadDir + urlObj.absoluteUrl, 'utf8', function (err, data) {
+      if (err) {
+        callback(err);
+      } else {
+        console.log('the hide file content:');
+        // console.log(data);
+        console.log(Buffer.isBuffer(data));
+        console.log(typeof data === "string");
+        var hideRelativeUrls =  data.match(/("|')[^('|")]+\.(js|css|html)(?='|")/g);
+        console.log('------------the hideRelativeUrls:--------------');
+        console.log(hideRelativeUrls);
+        var len = hideRelativeUrls.length;
+        while (len--) {
+          hideRelativeUrls[len] = hideRelativeUrls[len].slice(1);
+        }
+      callback(null, hideRelativeUrls);
+      }
+    });
+  }, function (err, urlsArray) {
+    if (err) {
+      callback(err);
+    } else {
+      let size = urlsArray.length;
+      // console.log('urlsArray size :');
+      // console.log(size);
+      let relativeUrls = [];
+      while (size--) {
+        relativeUrls = relativeUrls.concat(urlsArray[size]);
+      }
+      // console.log(relativeUrls);
+      callback(null, relativeUrls);
+    }
+  });
 }
 
-function makeDirsForRemain(downloadDir, remainUrlsRelative, callback) {
+function makeDirsForRemain(relativeUrls, callback) {
   console.log('makeDirsForRemain start');
-  callback(null, 222, 223);
+  let urlObjs = makeUrlObjs(hostPath, srcBase, relativeUrls);
+  async.each(urlObjs, function (urlObj, callback) {
+    let dirPath = downloadDir + urlObj.absoluteDir;
+    // console.log(dirPath);
+    mkdirs(dirPath, function (err) {
+      console.log('make remain Dir end' + dirPath);
+      callback(err)
+    });
+  }, function (err) {
+    if (err) {
+      callback(err);
+    } else {
+      console.log('makeDirsForRemain end');
+      callback(null, urlObjs)
+    }
+  });
 }
 
-function downloadRemain(downloadDir, remainUrlsRelative, callback) {
+function downloadRemain(urlObjs, callback) {
   console.log('downloadRemain start');
-  callback(null,'result');
+  download(downloadDir, limit, urlObjs, function (err) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
 }
 
 // 用相对地址创建对象数组，包含相对地址，绝对地址，绝对目录，完整url
@@ -198,12 +250,12 @@ function modifyAbsoDir(hostPath, srcBase, toBeModified) {
 function mkdirs(dirPath, callback) {
   fs.exists(dirPath, function (exist) {
     if (exist) {
-      console.log(dirPath + ' is existed.');
+      // console.log(dirPath + ' is existed.');
       callback(null);
       return;
     } else {
       mkdirs(path.dirname(dirPath), function () {
-        console.log('make Dir for ' + dirPath);
+        // console.log('make Dir for ' + dirPath);
         fs.mkdir(dirPath, callback);
       });
     }
@@ -211,36 +263,46 @@ function mkdirs(dirPath, callback) {
 }
 
 // 下载引用的文件
-function downloadFile(downloadObj) {
-  request(downloadObj.dir).pipe(fs.createWriteStream(downloadObj.url));
+// function downloadFile(downloadObj) {
+//   request(downloadObj.dir).pipe(fs.createWriteStream(downloadObj.url));
+// }
+
+function download(downloadDir, limit, urlObjs, callback) {
+
+  async.eachLimit(urlObjs, limit, function (urlObj, callback) {
+    let writer = fs.createWriteStream(downloadDir + urlObj.absoluteUrl);
+    request.get(urlObj.totalUrl)
+      .on('error', function (err) {
+        console.log('!!!!!!!!!!!!!!!!!!!!!!!!网络异常！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！');
+        callback(err);
+      })
+      .on('response', function (response) {
+        console.log('看看请求返回什么');
+        console.log(response.headers['content-type']);
+      })
+      .pipe(writer)
+      // .on('end',function () {
+      //   console.log('watching the end');
+      //   writer.end();
+      //   callback(null);
+      // });
+      .on('end', function () {
+        console.log('正在监听end事件');
+      })
+      .on('close', function () {
+        console.log('文件读取完成');
+        callback(null);
+      })
+    // callback(null);
+
+  }, function (err) {
+
+    if (err) {
+      callback(err);
+    } else {
+      console.log('files download complete.');
+      callback(null);
+    }
+
+  });
 }
-
-function download(urlObj) {
-
-}
-
-// superagent
-//   .get(target)
-//   .end(function (err, data) {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       indexPage = data.text;
-//       indexPageUrls = indexPage.match(/("|')[^('|")]+\.(js|css|html)(?='|")/g);
-//       // 取回来的indexPageUrls 第一个字符是 ' ，需要删除。
-//       let indexPageUrlsLen = indexPageUrls.length;
-//       while (indexPageUrlsLen--) {
-//         indexPageUrls[indexPageUrlsLen] = indexPageUrls[indexPageUrlsLen].slice(1);
-//       }
-//       console.log(indexPageUrls);
-//       fs.mkdir(downloadDir, function (err) {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           fs.writeFile(indexPageName, indexPage, function () {
-//
-//           })
-//         }
-//       })
-//     }
-//   });
